ff592d795986601dcdc7775a7924031d7cdf8967
Tdaycode <60110209+Tdaycode@users.noreply.github.com>
2020-03-26 18:54:23 +0100

    Add files via upload
    test COVID health state

---
+++ COVID-19 project/result.php
@@ -0,0 +1,48 @@
+<?php
+$cough1 =1;
+$cough2 =0;
+$cold1 =1;
+$cold2 =0;
+$diarrhea1 =1;
+$diarrhea2 =0;
+$sorethroat1 =1;
+$sorethroat2= 0;
+$bodyaches1 =1;
+$bodyaches2 =0;
+$headaches1 =1;
+$headaches2 =0;
+$fever1 =1;
+$fever2 =0;
+$breathing1 =2;
+$breathing2 =0;
+$fatigue1 =2;
+$fatigue2 =0;
+$travel1 =3;
+$travel2 =0;
+$covidarea1 =3;
+$covidarea2 =0;
+$patient1 =3;
+$patient2 =0;
+
+$Result = $cough1 + $cough2 + $cold1 + $cold2 + $diarrhea1 + $diarrhea2 + $sorethroat1 + $sorethroat2 +$bodyaches1 +
+$bodyaches2 +
+$headaches1 +
+$headaches2 +
+$fever1 +
+$fever2 +
+$breathing1 +
+$breathing2 +
+$fatigue1 +
+$fatigue2 +
+$travel1 +
+$travel2 +
+$covidarea1 +
+$covidarea2 +
+$patient1 +
+$patient2 ;
+ echo $Result
+
+
+
+
+?>
